22a08b86bfc2e4001dd18f74db441dd2441673a1
Diego Talledo Sanchez <85322967+DiegoTalledoS18@users.noreply.github.com>
2025-12-17 18:44:46 -0500

    add: header + footer

--- src/app/shared/layouts/public-layout/public-layout.ts
+++ src/app/shared/layouts/public-layout/public-layout.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-public-layout',
@@ -9,7 +10,9 @@ import {MatButtonModule} from '@angular/material/button';
     RouterOutlet,
     MatToolbarModule,
     MatButtonModule,
-    RouterLink
+    MatIconModule,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './public-layout.html',
   styleUrl: './public-layout.css',
