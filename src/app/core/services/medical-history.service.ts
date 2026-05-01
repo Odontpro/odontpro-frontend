6cfe4f83271129d43493b5099423a5bfbd5fe91f
Diego <diegotasa35@hotmail.com>
2026-05-01 17:49:55 -0500

    fix: api url

--- src/app/core/services/medical-history.service.ts
+++ src/app/core/services/medical-history.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {environment} from './environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicalHistoryService {
-  private readonly API_URL = 'http://localhost:3000/medical-history';
+  private readonly API_URL = `${environment.apiUrl}medical-history`;
 
   constructor(private http: HttpClient) {}
 
